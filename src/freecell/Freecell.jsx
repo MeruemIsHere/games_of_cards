5e2803f48a136bb869d83588dec45438a4c997e8
Johan <hoareau.johanjm@gmail.com>
2021-04-01 19:59:25 +0400

    create distribution function and render board

--- src/freecell/Freecell.jsx
+++ src/freecell/Freecell.jsx
@@ -1,18 +1,53 @@
 import React, { Component } from 'react';
 import shuffle from 'lodash.shuffle';
 import DeckConstructor from '../global_component/DeckConstructor';
+import './Freecell.css';
 
 class Freecell extends Component {
     state = {
         deck: shuffle(DeckConstructor()),
         board: [[], [], [], [], [], [], [], []],
     }
+    
+    componentDidMount(){
+        this.distribution()
+    }
+
+    distribution(){
+        let newBoard = this.state.board
+        this.state.deck.forEach(card => {
+            for(let i=0; i<8; i++){
+                if(i<4 && newBoard[i].length<7){
+                    newBoard[i] = [...newBoard[i], card]
+                    break
+                }
+                else if(i>=4 && newBoard[i].length<6){
+                    newBoard[i] = [...newBoard[i], card]
+                    break
+                }
+            }
+        })
+        this.setState({
+            board: newBoard
+        })
+    }
 
     render() {
-        const {deck, board} = this.state
+        const {board} = this.state
         return (
-            <div style={{display:"flex", flexWrap:"wrap" }}>
-                <p style={{width: "100%"}}>Test Freecell</p>
+            <div className="carpet">
+                <p>Test Freecell</p>
+                <div className="board">
+                    {board.map((stack, index) => (
+                        <div key={index} className="place-board">
+                            {stack.map((card, index) => (
+                                <div key={index} className="card" style={{top: `${index*30}px`}}>
+                                    <p className="value-card" style={{color: `${card.color}`}}>{card.value}{card.symbole}</p>
+                                </div>
+                            ))}
+                        </div>
+                    ))}
+                </div>
             </div>
         );
     }
