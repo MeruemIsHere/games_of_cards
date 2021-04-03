f000a006e75db91b72cf13e5602e93983c04849e
Johan <hoareau.johanjm@gmail.com>
2021-04-03 15:51:42 +0400

    Event -- Selection cards

--- src/freecell/Card.jsx
+++ src/freecell/Card.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Card = ({index, card, onClick}) => {
+const Card = ({index, stackIndex, card, onClick}) => {
     return(
-        <div className="card" onClick={onClick} style={{top: `${index * 30}px`}}>
+        <div className="card" onClick={ () => onClick(index, stackIndex)} style={{top: `${index * 30}px`}}>
             <p className="value-card" style={{color: `${card.color}`}}>{card.value}{card.symbole}</p>
         </div>
     )
--- src/freecell/Freecell.jsx
+++ src/freecell/Freecell.jsx
@@ -9,7 +9,18 @@ class Freecell extends Component {
         deck: shuffle(DeckConstructor()),
         board: [[], [], [], [], [], [], [], []],
         freePlaces: [[], [], [], []],
-        winPlaces: [[], [], [], []],
+        winPlaces: {
+            heart: [], 
+            diamond: [], 
+            club: [], 
+            spade: [],
+        },
+        cardsSelection: {
+            active: false,
+            stackSelected: [],
+            cards: [],
+            indexFirstCard: [],
+        },
     }
     
     componentDidMount(){
@@ -35,34 +46,114 @@ class Freecell extends Component {
         })
     }
 
+    selectCard = (cardIndex, stackIndex) => {
+        const {board, cardsSelection} = this.state
+        const newBoard = board
+
+        if(!cardsSelection.active){
+            let cardsSelected = board[stackIndex].slice(cardIndex)
+
+            // Plusieurs cartes sélectionnés
+            if(cardsSelected.length > 1){
+
+                //Controler si les valeurs cartes sont décroissant de 1
+                let values = cardsSelected.map(card => card.value)
+                let valuesDescending = [...values]
+                valuesDescending = valuesDescending.sort((a,b) => b - a)
+                let diffBetweenValues = []
+                for (let i = 1; i < valuesDescending.length; i++) {
+                    diffBetweenValues.push(valuesDescending[i - 1] - valuesDescending[i])
+                }
+
+                const isFollowingValues = !diffBetweenValues.some(diff => diff !== 1)
+                let isValuesDescending = values.every((value, index) => value === valuesDescending[index])
+
+                if(isValuesDescending && isFollowingValues) {
+                    //Controler si les couleurs sont alternés
+                    let colors = cardsSelected.map(card => card.color)
+                    let diffBetweenColors = []
+                    for (let i = 1; i < colors.length; i++) {
+                        if(colors[i] !== colors[i - 1]){
+                            diffBetweenColors.push(true)
+                        }
+                        else{
+                            diffBetweenColors.push(false)
+                        }
+                    }
+                    const isAlternateColors = !diffBetweenColors.includes(false)
+                    if (isAlternateColors) {
+                        this.setState({
+                            cardsSelection: {
+                                ...cardsSelection, 
+                                active: true, 
+                                stackSelected: stackIndex,
+                                cards: board[stackIndex].slice(cardIndex),
+                                indexFirstCard: [cardIndex],
+                            },
+                        })
+                    }
+                    else{
+                        console.log("Impossible")
+                    }
+                }
+                else{
+                    console.log("Impossible");
+                }
+
+            }
+        }
+
+        console.log(cardsSelection.cards)
+    }
+
+    // shiftingCards(cardIndex, stackIndex){
+    //     const stackSelected = board[stackIndex]
+    //     stackSelected.length= cardIndex
+    //     newBoard[stackIndex] = stackSelected
+
+    //     this.setState({
+    //         board: newBoard
+    //     })
+    // }
+
     render() {
-        const {board, freePlaces, winPlaces} = this.state
+        const {board, freePlaces, winPlaces, cardsSelection} = this.state
         return (
             <div className="carpet">
                 <p>Test Freecell</p>
                 <div className="top-board">
                     <div className="free-places">
                         {freePlaces.map((place, index) => (
-                            <div className="place"></div>
+                            <div className="place" key={index}>
+
+                            </div>
                         ))}
                     </div>
                     <div className="win-places">
-                        {winPlaces.map((place, index) => (
-                            <div className="place"></div>
-                        ))}
+                        <div className="place">{winPlaces.heart}</div>
+                        <div className="place">{winPlaces.diamond}</div>
+                        <div className="place">{winPlaces.club}</div>
+                        <div className="place">{winPlaces.spade}</div>
                     </div>
                 </div>
                 <div className="board">
-                    {board.map((stack, index) => (
-                        <div key={index} className="place">
-                            {stack.map((card, index) => (
+                    {board.map((stack, stackIndex) => (
+                        <div key={stackIndex} className="place">
+                            {stack.map((card, cardIndex) => (
                                 <Card 
-                                    key={index} 
-                                    index={index} 
+                                    key={cardIndex} 
+                                    index={cardIndex} 
+                                    stackIndex={stackIndex}
                                     card={card}
-                                    onClick={this.shifting}
+                                    onClick={this.selectCard}
                                 />
                             ))}
+                            {cardsSelection.active && (cardsSelection.stackSelected === stackIndex) ? 
+                                <div className="selection" style={{top: `${cardsSelection.indexFirstCard * 30}px`, height: `${(cardsSelection.cards.length * 30) + 40}px`}}>
+                                </div> 
+                                
+                                : null
+                            }
                         </div>
                     ))}
                 </div>
