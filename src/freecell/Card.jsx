1955bf0f0db42a09d571a41810356f99528517bd
Johan <hoareau.johanjm@gmail.com>
2021-04-09 09:19:20 +0400

    Import img folder and config bg-Img for Card.jsx

--- src/freecell/Card.jsx
+++ src/freecell/Card.jsx
@@ -2,18 +2,32 @@ import React from 'react';
 
 
 const Card = ({index, stackIndex, place, card, onClick}) => {
-    let positionCard = {}
+    let symbole = ""
+    if(card.symbole === "♥️") { symbole = "heart" }
+    else if(card.symbole === "♦️") { symbole = "diamond" }
+    else if(card.symbole === "♠️") { symbole = "spade" }
+    else if(card.symbole === "♣️") { symbole = "club" }
+    const value = `${card.value}`
+    const pathImg = `/img/${symbole}/${value}.png`
+    let positionCard = {
+        backgroundImage: `url(${process.env.PUBLIC_URL + pathImg})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+    }
     if (place === "win"){
-        positionCard = {top: `0px`}
+        positionCard = {...positionCard, top: `0px`}
     }
     else {
-        positionCard = {top: `${index * 30}px`}
+        positionCard = {...positionCard, top: `${index * 30}px`}
     }
+
     
     return(
-        <div className="card" onClick={ () => place === "win" ? null : onClick(index, stackIndex, place)} style={positionCard}>
-            <p className="value-card" style={{color: `${card.color}`}}>{card.value}{card.symbole}</p>
-        </div>
+        <div 
+            className="card" 
+            onClick={ () => place === "win" ? null : onClick(index, stackIndex, place)} 
+            style={positionCard}
+        ></div>
     )
 }
 
